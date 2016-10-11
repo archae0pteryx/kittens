9bd95ef6a41845d04ccff15fed87973f0125fe88
archae0pteryx <jesus.christ@openmailbox.org>
2016-10-11 09:36:53 -0700

    kik

--- core/in.sh
+++ core/in.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 #
 # @archae0pteryx
-./core/checks.sh
-./core/ups.sh
-./ins/in_base.sh
-./ins/in_db.sh
-./ins/in_net.sh
-./ins/in_srv.sh
-./sets/set_ssh.sh
-./sets/set_ufw.sh
-./sets/set_user.sh
--- core/runall.sh
+++ core/runall.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 clear
+source vars
 echo "runall [+]"
 sleep 1
+./core/checks.sh
+./core/ups.sh
+./ins/in_base.sh
+./ins/in_db.sh
+./ins/in_net.sh
+./ins/in_srv.sh
+./sets/set_ssh.sh
+./sets/set_ufw.sh
+./sets/set_user.sh
